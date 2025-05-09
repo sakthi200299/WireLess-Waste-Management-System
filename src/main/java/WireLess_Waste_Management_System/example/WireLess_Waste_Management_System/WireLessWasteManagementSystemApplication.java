8243a3a1c8ff6b31e34601119f2a6f9fa1a4a665
Sakthivel R <sv212766@gmail.com>
2025-05-09 17:46:03 +0530

    basic setup

---
+++ src/main/java/WireLess_Waste_Management_System/example/WireLess_Waste_Management_System/WireLessWasteManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package WireLess_Waste_Management_System.example.WireLess_Waste_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WireLessWasteManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WireLessWasteManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WireLess_Waste_Management_System/example/WireLess_Waste_Management_System/WireLessWasteManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package WireLess_Waste_Management_System.example.WireLess_Waste_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WireLessWasteManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
